54f648f2fce3f39a640335f749e14952af54a126
Artem Kornilaev <artemkornilaev1@gmail.com>
2025-04-23 21:25:37 +0400

    init commit

---
+++ internal/repo/contracts.go
@@ -0,0 +1,4 @@
+package repo
+
+type AuthRepository interface {
+}
---
+++ internal/repo/user_postgres.go
@@ -0,0 +1,7 @@
+package repo
+
+import "github.com/keshvan/go-common-forum/postgres"
+
+type userRepository struct {
+	pg *postgres.Postgres
+}
